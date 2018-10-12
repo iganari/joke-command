345ced09b894cfe4cef0ff3bf75fd26eaebebc5b
iganari <cigaguri@gmail.com>
2018-10-12 15:50:18 +0900

    統合

--- docker-build-run.sh
+++ docker-build-run.sh
@@ -5,3 +5,14 @@ set -x
 _I_TAG='u-xenia'
 docker build . -t  ${_I_TAG}
 sh ./docker-run.sh ${_I_TAG}
+#!/bin/bash
+
+set -x
+
+BASEPATH=$(cd `dirname $0`; pwd)
+
+docker run --rm -it -v ${BASEPATH}:/usr/local/iganari \
+                    -w /usr/local/iganari \
+                    --name ${1} \
+                    ${1} \
+/bin/bash
